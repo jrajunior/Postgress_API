b4e4785cd021eee3fc0f349e8fc81d96a77010e2
Jose Andrade <jrajunior@gmail.com>
2023-07-25 15:02:27 +1000

    Post + Get with List over Tuple

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 
 app = Flask(__name__) # Use __name__ as the import_name argument
@@ -27,14 +27,24 @@ def get_data():
         query = 'SELECT * FROM public."customers";'
         cursor.execute(query)
         customers = cursor.fetchall()
+
         if customers:
             result = []
             for customer in customers:
-                result.append({"id": customer.id[0], "first_name": customer.first_name[1], "last_name": customer.last_name[2], "email": customer.email[3]})
+                # Convert the tuple to a list using list()
+                customer_list = list(customer)
+
+                # Access data from the list using index positions
+                result.append({
+                    "id": customer_list[0],         # Access the first column (id)
+                    "first_name": customer_list[1], # Access the second column (first_name)
+                    "last_name": customer_list[2],  # Access the third column (last_name)
+                    "email": customer_list[3]       # Access the fourth column (email)
+                })
             return jsonify(result)
         else:
             return jsonify({"error": f"Customers not found."}), 404
-        
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -42,6 +52,35 @@ def get_data():
 def hello():
 	return "Hello, world!"
 
+@app.route('/create', methods=['POST'])
+def create():
+    # Get the first_name, last_name and email from the request body
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+
+    try:
+        # Insert the data into the database
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute(
+            "INSERT INTO public.\"customers\" (first_name, last_name, email) VALUES (%s, %s, %s)", 
+            (first_name, last_name, email)
+        )
+        connection.commit()
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+        return {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "message": f"Customer {first_name} created."
+        }, 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 if __name__ == '__main__':
         app.run(debug=True, port=8000)
-
